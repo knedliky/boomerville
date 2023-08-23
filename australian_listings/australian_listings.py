7d736007034ece06e72fe2117e28967d7d70bd8c
Simon <35138806+knedliky@users.noreply.github.com>
2023-08-23 17:39:01 +1000

    Synchronizing job australian_listings from AWS Glue at 2023/08/23 07:39:01

--- australian_listings/australian_listings.py
+++ australian_listings/australian_listings.py
@@ -1,41 +1,77 @@
 
 import sys
+import io
+import pandas as pd 
+import requests
+import base64
+import boto3
+from datetime import datetime
+
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-  
+
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-dyf = glueContext.create_dynamic_frame.from_catalog(database='database_name', table_name='table_name')
-dyf.printSchema()
-df = dyf.toDF()
-df.show()
-import matplotlib.pyplot as plt
+client_id = "client_0b205b5e26b46b96c3d81cc0aeb71cd3"
+client_secret = "secret_d89b759bd6e47de21d7c064a4f307657"
 
-# Set X-axis and Y-axis values
-x = [5, 2, 8, 4, 9]
-y = [10, 4, 8, 5, 2]
-  
-# Create a bar chart 
-plt.bar(x, y)
-  
-# Show the plot
-s3output = glueContext.getSink(
-  path="s3://bucket_name/folder_name",
-  connection_type="s3",
-  updateBehavior="UPDATE_IN_DATABASE",
-  partitionKeys=[],
-  compression="snappy",
-  enableUpdateCatalog=True,
-  transformation_ctx="s3output",
-)
-s3output.setCatalogInfo(
-  catalogDatabase="demo", catalogTableName="populations"
-)
-s3output.setFormat("glueparquet")
-s3output.writeFrame(DyF)
+def encode_credentials(): 
+    client_credentials = f"{client_id}:{client_secret}"
+    client_credentials_b64 = base64.b64encode(client_credentials.encode())
+    return client_credentials_b64.decode()
+
+def get_token():
+    url = 'https://auth.domain.com.au/v1/connect/token'
+    headers = {
+    'Authorization': f'Basic {encode_credentials()}'
+    }
+    data = {
+    'grant_type': 'client_credentials',
+    'scope': 'api_listings_read api_salesresults_read'
+    }
+    r = requests.post(url, headers=headers, data=data)
+    return r.json()['access_token']
+
+def get_listings(city:str):
+    url = f'https://api.domain.com.au/v1/salesResults/{city}/listings'
+
+    headers = {
+        'Authorization': f'Bearer {get_token()}'
+    }
+    response = requests.get(url, headers=headers)
+    return response
+
+def response_to_csv(response:str):
+    return pd.read_json(io.StringIO(response.text))
+
+cities = ["Melbourne", "Sydney", "Canberra", "Adelaide", "Brisbane"]
+
+df = pd.concat([response_to_csv(get_listings(city)) for city  in cities])
+# Drop duplicates
+df = df.drop_duplicates(subset='id')
+
+# Unpack GeoLocation
+df[['latitude', 'longitude']] = pd.json_normalize(df['geoLocation'])
+df = df.drop(columns='geoLocation')
+year = datetime.now().year
+month = datetime.now().month
+date = datetime.now().strftime('%Y-%m-%d')
+
+bucket_name = 'knedliky-bronze'
+object_key = f'domain/{year}/{month}/australian_house_prices_{date}.csv'
+
+# buffer = io.BytesIO()
+# df.to_csv(buffer, index=False)
+# bytes = buffer.getvalue().encode('utf8')
+csv = df.to_csv(index=False)
+bytes = csv.encode()
+
+s3 = boto3.client('s3')
+s3.put_object(Bucket=bucket_name, Key=object_key, Body=bytes)
+bytes
 job.commit()
